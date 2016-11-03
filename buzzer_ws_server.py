d93e59e28096ffaf26523eb65d4e336b5c6ddd34
Matthew Kokotovich <mkokotovich@gmail.com>
2016-11-03 11:54:34 -0400

    Fixed the default address, fixed the reset method

--- buzzer_ws_server.py
+++ buzzer_ws_server.py
@@ -2,6 +2,7 @@
 import argparse
 import random
 import os
+import socket
 
 import cherrypy
 
@@ -15,12 +16,17 @@ gteams = []
 class BuzzerSocketHandler(WebSocket):
     def received_message(self, m):
         global gteams
-        team = str(m)
-        if team not in gteams:
-            cherrypy.log("Team added : %s" % team)
-            gteams.append(team)
-        msg = "<h1>Teams have buzzed:</h1>"
-        msg += "<br>".join("Team: " + t for t in gteams)
+        m_str = str(m)
+        if m_str == "###reset###":
+            gteams = []
+            msg = "<h1>Ready to buzz!</h1>"
+        else:
+	    team = m_str
+	    if team not in gteams:
+	        cherrypy.log("Team added : %s" % team)
+	        gteams.append(team)
+	    msg = "<h1>Teams have buzzed:</h1>"
+	    msg += "<br>".join("Team: " + t for t in gteams)
         cherrypy.engine.publish('websocket-broadcast', msg)
 
 class BuzzerServer(object):
@@ -55,16 +61,23 @@ class BuzzerServer(object):
              ws.send($('#teamname').val());
              return false;
           });
+          $('#reset').click(function() {
+             ws.send('###reset###');
+             return false;
+          });
         });
       </script>
     </head>
     <body>
-        <a href="/reset">Reset buzzer</a>
     <form action='#' id='chatform' method='get'>
-      <label id='output'><h1>Ready to buzz!</h1></textarea>
+      <label id='output'><h1>Ready to buzz!</h1></label>
+      <br />
       <br />
       <label for='message'>Team name: </label><input type='text' id='teamname' />
-      <input id='buzz' type='submit' value='Buzz!' />
+      <input id='buzz' type='submit' value='    Buzz!    ' />
+      <br />
+      <br />
+      <input id='reset' type='submit' value='Reset Buzzer' />
       </form>
     </body>
     </html>
@@ -85,12 +98,14 @@ if __name__ == '__main__':
     from ws4py import configure_logger
     configure_logger(level=logging.DEBUG)
 
+    default_address = [(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+
     parser = argparse.ArgumentParser(description='Game Show Buzzer')
-    parser.add_argument('--host', default='localhost')
+    parser.add_argument('--host', default=default_address)
     parser.add_argument('-p', '--port', default=9000, type=int)
     args = parser.parse_args()
 
-    cherrypy.config.update({'server.socket_host': args.host,
+    cherrypy.config.update({'server.socket_host': '0.0.0.0',
                             'server.socket_port': args.port,
                             'tools.staticdir.root': os.path.abspath(os.path.join(os.path.dirname(__file__), 'static'))})
 
